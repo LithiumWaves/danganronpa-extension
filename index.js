b675169aab3fc5322e341bfe2ec5768fcddc5513
LithiumWaves <pressstart132@gmail.com>
2026-01-16 17:42:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 import { extension_settings } from "../../../extensions.js";
 import { saveSettingsDebounced } from "../../../../script.js";
 
-// MUST match folder name exactly
 const extensionName = "danganronpa-extension";
 const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
+
+const defaultSettings = {
+    enabled: false
+};
+
+function loadSettings() {
+    extension_settings[extensionName] = extension_settings[extensionName] || {};
+
+    if (Object.keys(extension_settings[extensionName]).length === 0) {
+        Object.assign(extension_settings[extensionName], defaultSettings);
+    }
+
+    $("#dangan_enable_checkbox").prop(
+        "checked",
+        extension_settings[extensionName].enabled
+    );
+}
+
+function onCheckboxChange(event) {
+    const value = Boolean($(event.target).prop("checked"));
+    extension_settings[extensionName].enabled = value;
+    saveSettingsDebounced();
+
+    console.log(`[${extensionName}] enabled =`, value);
+}
 
 jQuery(async () => {
     console.log(`[${extensionName}] Loading...`);
@@ -11,6 +35,10 @@ jQuery(async () => {
     try {
         const settingsHtml = await $.get(`${extensionFolderPath}/example.html`);
         $("#extensions_settings2").append(settingsHtml);
+
+        $("#dangan_enable_checkbox").on("input", onCheckboxChange);
+
+        loadSettings();
 
         console.log(`[${extensionName}] ✅ Loaded successfully`);
     } catch (error) {
